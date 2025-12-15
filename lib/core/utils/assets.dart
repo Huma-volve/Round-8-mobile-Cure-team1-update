6bcddfb93356df424bb034846e0f648726cdbb03
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-12-15 16:42:54 +0200

    solve issuse

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,37 +1,40 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
+  
+  static const String resourceImagesActiveCheckedIcon = "assets/images/active_checked_icon.png";
 
-  static const String resourceImagesActiveCheckedIcon =
-      "assets/images/active_checked_icon.png";
-
-  static const String resourceImagesActiveStar =
-      "assets/images/active_star.png";
+  static const String resourceImagesActiveStar = "assets/images/active_star.png";
 
   static const String resourceImagesAddReview = "assets/images/add_review.png";
 
   static const String resourceImagesAlert02 = "assets/images/alert-02.png";
 
-  static const String resourceImagesAltArrowDown =
-      "assets/images/Alt_Arrow_Down.png";
+  static const String resourceImagesAltArrowDown = "assets/images/Alt_Arrow_Down.png";
 
   static const String resourceImagesApplePay = "assets/images/apple-pay.png";
 
-  static const String resourceImagesArrowImage =
-      "assets/images/arrow_image.png";
+  static const String resourceImagesArrowImage = "assets/images/arrow_image.png";
 
   static const String resourceImagesArrowLeft = "assets/images/arrow-left.png";
 
-  static const String resourceImagesArrowRight =
-      "assets/images/arrow-right.png";
+  static const String resourceImagesArrowRight = "assets/images/arrow-right.png";
 
-  static const String resourceImagesCalendar02 =
-      "assets/images/calendar-02.png";
+  static const String resourceImagesBackarrow = "assets/images/backarrow.svg";
+
+  static const String resourceImagesCalendar = "assets/images/calendar.svg";
+
+  static const String resourceImagesCalendar02 = "assets/images/calendar-02.png";
+
+  static const String resourceImagesCall = "assets/images/call.svg";
 
   static const String resourceImagesCamera = "assets/images/Camera.svg";
 
-  static const String resourceImagesCheckedImage =
-      "assets/images/checked_image.png";
+  static const String resourceImagesCard  = "assets/images/Card (1).png";
+
+  static const String resourceImagesChat = "assets/images/chat.svg";
+
+  static const String resourceImagesCheckedImage = "assets/images/checked_image.png";
 
   static const String resourceImagesDoctor = "assets/images/doctor.png";
 
@@ -43,14 +46,19 @@ class Assets {
 
   static const String resourceImagesFavorite = "assets/images/favorite.png";
 
-  static const String resourceImagesIconGoogle =
-      "assets/images/icon_google.png";
+  static const String resourceImagesGroup = "assets/images/Group 7 (1).png";
 
-  static const String resourceImagesIconSplash =
-      "assets/images/icon_splash.png";
+  static const String resourceImagesHome = "assets/images/Home.svg";
 
-  static const String resourceImagesInActiveStar =
-      "assets/images/in_active_star.png";
+  static const String resourceImagesHstory = "assets/images/hstory.svg";
+
+  static const String resourceImagesIconGoogle = "assets/images/icon_google.png";
+
+  static const String resourceImagesIconSplash = "assets/images/icon_splash.png";
+
+  static const String resourceImagesImage  = "assets/images/image 44.png";
+
+  static const String resourceImagesInActiveStar = "assets/images/in_active_star.png";
 
   static const String resourceImagesJana = "assets/images/jana.png";
 
@@ -60,33 +68,77 @@ class Assets {
 
   static const String resourceImagesLogin = "assets/images/login.png";
 
+  static const String resourceImagesMagnifer = "assets/images/Magnifer.svg";
+
   static const String resourceImagesMedal = "assets/images/medal.png";
 
   static const String resourceImagesMessage = "assets/images/message.png";
 
   static const String resourceImagesMic = "assets/images/mic.svg";
 
-  static const String resourceImagesOnboarding1 =
-      "assets/images/onboarding1.png";
+  static const String resourceImagesOnboarding1 = "assets/images/onboarding1.png";
 
-  static const String resourceImagesOnboarding2 =
-      "assets/images/onboarding2.png";
+  static const String resourceImagesOnboarding2 = "assets/images/onboarding2.png";
 
   static const String resourceImagesPaperclip = "assets/images/Paperclip.svg";
+
+  static const String resourceImagesPaymentIcOutlinePaypalPayment = "assets/images/payment/ic_outline-paypal_payment.svg";
+
+  static const String resourceImagesPaymentLogosApplePayPayment = "assets/images/payment/logos_apple-pay_payment.svg";
+
+  static const String resourceImagesPaymentVisaPayment = "assets/images/payment/visa_payment.svg";
 
   static const String resourceImagesPaypal = "assets/images/paypal.png";
 
   static const String resourceImagesPin = "assets/images/Pin.svg";
 
-  static const String resourceImagesProfile2user =
-      "assets/images/profile-2user.png";
+  static const String resourceImagesProfile = "assets/images/profile.svg";
+
+  static const String resourceImagesProfile2user = "assets/images/profile-2user.png";
+
+  static const String resourceImagesProfileArrowRightProfile = "assets/images/profile/arrow_right_profile.svg";
+
+  static const String resourceImagesProfileBanknoteProfile = "assets/images/profile/Banknote_profile.svg";
+
+  static const String resourceImagesProfileCameraEditProfile = "assets/images/profile/camera_edit_profile.svg";
+
+  static const String resourceImagesProfileChatProfile = "assets/images/profile/Chat_profile.svg";
+
+  static const String resourceImagesProfileFamiconsPersonEditProfile = "assets/images/profile/famicons_person_edit_profile.svg";
+
+  static const String resourceImagesProfileFlagEgyptEditProfile = "assets/images/profile/Flag_egypt_edit_profile.svg";
+
+  static const String resourceImagesProfileGroupAddCardProfile = "assets/images/profile/Group_add_card_profile.svg";
+
+  static const String resourceImagesProfileHeartProfile = "assets/images/profile/Heart_profile.svg";
+
+  static const String resourceImagesProfileLogoutProfile = "assets/images/profile/Logout_Profile.svg";
+
+  static const String resourceImagesProfileMdiEmailEditProfile = "assets/images/profile/mdi_email_edit_profile.svg";
+
+  static const String resourceImagesProfileNotificationProfile = "assets/images/profile/notification_profile.svg";
+
+  static const String resourceImagesProfileProfile = "assets/images/profile/Profile.svg";
+
+  static const String resourceImagesProfileSettingsProfile = "assets/images/profile/Settings_profile.svg";
 
   static const String resourceImagesReviews = "assets/images/reviews.png";
 
   static const String resourceImagesRobert = "assets/images/robert.png";
 
-  static const String resourceImagesSolarCheckCircle =
-      "assets/images/solar_check_circle.png";
+  static const String resourceImagesSittingsKey  = "assets/images/sittings/Key Square_settings.svg";
+
+  static const String resourceImagesSittingsLock = "assets/images/sittings/Lock Keyhole Minimalistic_sittings.svg";
+
+  static const String resourceImagesSittingsMaterialSymbolsAddRoundedFAQ = "assets/images/sittings/material-symbols_add-rounded_FAQ.svg";
+
+  static const String resourceImagesSittingsMinusSignFAQ = "assets/images/sittings/minus-sign_FAQ.svg";
+
+  static const String resourceImagesSittingsOutlineVisiblePasswordSittings = "assets/images/sittings/Outline_visible_password_sittings.svg";
+
+  static const String resourceImagesSittingsUser  = "assets/images/sittings/User Rounded_sittings.svg";
+
+  static const String resourceImagesSolarCheckCircle = "assets/images/solar_check_circle.png";
 
   static const String resourceImagesSplashDot = "assets/images/splash_dot.png";
 
@@ -98,18 +150,17 @@ class Assets {
 
   static const String resourceImagesTime = "assets/images/time.png";
 
-  static const String resourceImagesTrashBinTrash =
-      "assets/images/Trash_Bin_Trash.svg";
+  static const String resourceImagesTrashBinTrash = "assets/images/Trash_Bin_Trash.svg";
 
   static const String resourceImagesTurner = "assets/images/turner.png";
 
-  static const String resourceImagesUserComment =
-      "assets/images/user_comment.png";
+  static const String resourceImagesUserComment = "assets/images/user_comment.png";
+
+  static const String resourceImagesVideocamera = "assets/images/Videocamera.svg";
 
   static const String resourceImagesVisa = "assets/images/visa.png";
 
-  static const String resourceImagesVolumeCross =
-      "assets/images/Volume_Cross.svg";
+  static const String resourceImagesVolumeCross = "assets/images/Volume_Cross.svg";
 
   static const String resourceImagesWarning = "assets/images/warning.png";
 
@@ -161,3 +212,4 @@ class Assets {
       "assets/images/sittings/minus-sign_FAQ.svg";
 //another
 }
+
